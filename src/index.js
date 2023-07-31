bbaf6f58ecd65fdb5cd98716c287b30ccc9d315b
KUN1007 <yuyuyukunkunkun@gmail.com>
2023-07-31 14:47:44 +0800

    init: create project frame

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const Koa = require('koa')
+
+const app = new Koa()
+
+app.use((ctx, next) => {
+  ctx.body = 'KUN IS CUTEST!'
+})
+
+app.listen(3000, () => {
+  console.log('server is running on http://localhost:3000')
+})
